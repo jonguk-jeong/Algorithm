bbaf15b2d7537ae2d88679dcd9afd146307a8a4f
깃허브아이디 <>
2026-02-27 18:03:29 +0900

    solve: BOJ 17608 막대기

---
+++ src/BOJ/_17608/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] h = new int[n];
+
+        for (int i = 0; i <= n; i++) {
+            h[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (h[i] > max) {
+                max = h[i];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}

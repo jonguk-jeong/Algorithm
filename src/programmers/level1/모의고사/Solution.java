55ae72edd170afb5092ad27bcff68cbe9ac52538
깃허브아이디 <138849418+jonguk-jeong@users.noreply.github.com>
2026-03-15 11:54:55 +0900

    solve: programmers 모의고사

---
+++ src/programmers/level1/모의고사/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] p1 = {1,2,3,4,5};
+        int[] p2 = {2,1,2,3,2,4,2,5};
+        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
+
+        int[] score = {0,0,0};
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == p1[i%5]) score[0]++;
+            if (answers[i] == p2[i%8]) score[1]++;
+            if (answers[i] == p3[i%10]) score[2]++;
+        }
+
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            if (max == score[i]) list.add(i+1);
+        }
+
+        int[] sol = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            sol[i] = list.get(i);
+        }
+
+        return sol;
+    }
+}

da82f21847cf5dd7f12e012dfbef77c4ce34e3e0
jonguk-jeong <138849418+jonguk-jeong@users.noreply.github.com>
2026-04-02 18:12:13 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                for (int k = 0; k < i; k++) {
+                    list.add(str_list[k]);
+                }
+                break;
+            } else if (str_list[i].equals("r")) {
+                for (int k = i+1; k < str_list.length; k++) {
+                    list.add(str_list[k]);
+                }
+                break;
+            }
+        }
+        
+        if (list.isEmpty()) {
+            String[] answer = {};
+            return answer;
+        }
+        
+        String[] answer = new String[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}

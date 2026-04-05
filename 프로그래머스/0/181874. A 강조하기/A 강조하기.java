f9ba80e3ccd87cb78a170e32039c035f7db09bb7
jonguk-jeong <138849418+jonguk-jeong@users.noreply.github.com>
2026-04-05 09:33:41 +0900

    [level 0] Title: A 강조하기, Time: 2.10 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for (int i = 0; i < myString.length(); i++) {
+            String c = myString.substring(i, i+1);
+            
+            if (c.equals("a") || c.equals("A")) {
+                answer += "A";
+            } else {
+                answer += c.toLowerCase();
+            }
+        }
+        
+        return answer;
+    }
+}

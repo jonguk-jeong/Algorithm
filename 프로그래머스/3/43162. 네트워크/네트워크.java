c292d8dd52a7e88468c32895d3e87fd647b0d959
jonguk-jeong <138849418+jonguk-jeong@users.noreply.github.com>
2026-05-07 21:40:34 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,24 @@
-class Solution {
-    boolean[] visited;
-    
+class Solution {    
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
+        boolean visited[] = new boolean[n];
         int answer = 0;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(computers, n, i);
+                dfs(computers, visited, i);
                 answer++;
             }
         }
-        
-        
         return answer;
     }
     
-    public void dfs(int[][] computers, int n, int node) {
+    public void dfs(int[][] computers, boolean[] visited, int node) {
+        visited[node] = true;
         
-        visited[node] = true;        
-        
-        for (int i = 0; i < n; i++) {
-            if (computers[node][i] == 1 && visited[i] == false) {
-                dfs(computers, n, i);
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[node][i] == 1 && !visited[i]) {
+                dfs(computers, visited, i);
             }
         }
-        
     }
 }
